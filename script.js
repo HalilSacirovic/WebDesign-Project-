10ece1c3add0f0fab328b8afa49bccd3f2d01992
HalilSacirovic <halilsacirovic03@gmail.com>
2023-11-24 16:10:01 +0100

    a

--- script.js
+++ script.js
@@ -2,97 +2,97 @@
 
 
 
-var array = [];
-var array2 =[];
-
-
-const getUsers =()=>{
-    fetch('https://dummyjson.com/users')
-    .then(res => res.json())
-    .then((data)=>{
-        array =data.users;
-        array2= data.users
-        renderUsers()
-    });
-}
-
-
-const renderUsers = () =>{
-    const ul= document.getElementById("ul");
-    ul.innerHTML ="";
-    array.forEach((item,index)=>{
-        const li = document.createElement("li");
-        const p = document.createElement("p");
-        
-        p.textContent = item.firstName + " " + item.lastName + ` is ${item.age} years old`;
-        
-        li.appendChild(p);
-        ul.appendChild(li);
-        
-    })
-}
-
-getUsers()
-document.getElementById("search").addEventListener("keydown",()=>{
-    const search_value =   document.getElementById("search").value;
-    array = array2.filter((item,index)=>{
-        if(item.firstName)
-        {
-            return item.firstName.toLowerCase().includes(search_value.toLowerCase())
-        }
-     
-    })
-    renderUsers()
-})
-
-// DRUGI NACIN 
-
 // var array = [];
+// var array2 =[];
 
-// const getUsers = () => {
+
+// const getUsers =()=>{
 //     fetch('https://dummyjson.com/users')
-//         .then(res => res.json())
-//         .then((data) => {
-//             array = data.users;
-//             renderUsers();
-//         });
+//     .then(res => res.json())
+//     .then((data)=>{
+//         array =data.users;
+//         array2= data.users
+//         renderUsers()
+//     });
 // }
 
-// const renderUsers = () => {
-//     const ul = document.getElementById("ul");
-//     ul.innerHTML = "";
-//     array.forEach((item, index) => {
+
+// const renderUsers = () =>{
+//     const ul= document.getElementById("ul");
+//     ul.innerHTML ="";
+//     array.forEach((item,index)=>{
 //         const li = document.createElement("li");
 //         const p = document.createElement("p");
-
+        
 //         p.textContent = item.firstName + " " + item.lastName + ` is ${item.age} years old`;
-
+        
 //         li.appendChild(p);
 //         ul.appendChild(li);
-
+        
 //     })
 // }
 
-// getUsers();
+// getUsers()
+// document.getElementById("search").addEventListener("keydown",()=>{
+//     const search_value =   document.getElementById("search").value;
+//     array = array2.filter((item,index)=>{
+//         if(item.firstName)
+//         {
+//             return item.firstName.toLowerCase().includes(search_value.toLowerCase())
+//         }
+     
+//     })
+//     renderUsers()
+// })
 
-// document.getElementById("search").addEventListener("input", () => {
-//     const search_value = document.getElementById("search").value.toLowerCase();
-//     const filteredArray = array.filter((item) => {
-//         return item.firstName && item.firstName.toLowerCase().includes(search_value);
-//     });
-//     renderFilteredUsers(filteredArray);
-// });
+// DRUGI NACIN 
 
-// const renderFilteredUsers = (filteredArray) => {
-//     const ul = document.getElementById("ul");
-//     ul.innerHTML = "";
-//     filteredArray.forEach((item, index) => {
-//         const li = document.createElement("li");
-//         const p = document.createElement("p");
+var array = [];
 
-//         p.textContent = item.firstName + " " + item.lastName + ` is ${item.age} years old`;
+const getUsers = () => {
+    fetch('https://dummyjson.com/users')
+        .then(res => res.json())
+        .then((data) => {
+            array = data.users;
+            renderUsers();
+        });
+}
 
-//         li.appendChild(p);
-//         ul.appendChild(li);
-//     });
-// }
+const renderUsers = () => {
+    const ul = document.getElementById("ul");
+    ul.innerHTML = "";
+    array.forEach((item, index) => {
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+
+        p.textContent = item.firstName + " " + item.lastName + ` is ${item.age} years old`;
+
+        li.appendChild(p);
+        ul.appendChild(li);
+
+    })
+}
+
+getUsers();
+
+document.getElementById("search").addEventListener("input", () => {
+    const search_value = document.getElementById("search").value.toLowerCase();
+    const filteredArray = array.filter((item) => {
+        return item.firstName && item.firstName.toLowerCase().includes(search_value);
+    });
+    renderFilteredUsers(filteredArray);
+});
+
+const renderFilteredUsers = (filteredArray) => {
+    const ul = document.getElementById("ul");
+    ul.innerHTML = "";
+    filteredArray.forEach((item, index) => {
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+
+        p.textContent = item.firstName + " " + item.lastName + ` is ${item.age} years old`;
+
+        li.appendChild(p);
+        ul.appendChild(li);
+    });
+}
